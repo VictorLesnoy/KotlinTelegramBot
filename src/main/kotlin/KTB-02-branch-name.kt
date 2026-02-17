ce81adee937202ab68206a0f4a881bf3a69f3d59
VictorLesnoy <martynovv@list.ru>
2026-02-17 20:22:54 +0500

    KTB-02-branch-name

---
+++ src/main/kotlin/KTB-02-branch-name.kt
@@ -0,0 +1,13 @@
+package org.example
+
+import java.io.File
+
+fun main() {
+    val file = File("words.txt")
+
+    val lines = file.readLines()
+
+    for (line in lines) {
+        println(line)
+    }
+}
