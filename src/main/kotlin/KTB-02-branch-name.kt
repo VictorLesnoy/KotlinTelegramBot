173b013c679783dcb59477e53f7911050e2e6ace
VictorLesnoy <martynovv@list.ru>
2026-02-18 21:24:08 +0500

    KTB-02-branch-name

--- src/main/kotlin/KTB-02-branch-name.kt
+++ src/main/kotlin/KTB-02-branch-name.kt
@@ -1,13 +1,17 @@
 package org.example
 
 import java.io.File
+import java.io.IOException
 
 fun main() {
     val file = File("words.txt")
 
-    val lines = file.readLines()
-
-    for (line in lines) {
-        println(line)
+    try {
+        val lines = file.readLines()
+        for (line in lines) {
+            println(line)
+        }
+    } catch (e: IOException) {
+        println("Не найден файл ${e.message}.")
     }
 }
