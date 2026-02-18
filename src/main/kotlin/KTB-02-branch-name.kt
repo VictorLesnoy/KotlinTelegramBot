ba4ef02aac917e6288d87b630b2a836fc1040c02
VictorLesnoy <martynovv@list.ru>
2026-02-18 22:49:48 +0500

    KTB-02-branch-name

--- src/main/kotlin/KTB-02-branch-name.kt
+++ src/main/kotlin/KTB-02-branch-name.kt
@@ -3,15 +3,22 @@ package org.example
 import java.io.File
 import java.io.IOException
 
+data class Word(val original: String,
+                val translate: String,
+                val correctAnswersCount: Int = 0)
+
 fun main() {
-    val file = File("words.txt")
+    val wordsFile: File = File("words.txt")
 
     try {
-        val lines = file.readLines()
+        val lines: List<String> = wordsFile.readLines()
         for (line in lines) {
-            println(line)
+            val dictionary = line.split("|")
+            val word = Word(original = dictionary[0], translate = dictionary[1])
+            println(dictionary)
         }
     } catch (e: IOException) {
         println("Не найден файл ${e.message}.")
     }
+
 }
