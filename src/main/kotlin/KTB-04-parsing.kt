5d9975f040b503b98859ebf7bfecc5a5c4c0e75a
VictorLesnoy <martynovv@list.ru>
2026-02-19 21:11:27 +0500

    KTB-04-parsing

--- src/main/kotlin/KTB-04-parsing.kt
+++ src/main/kotlin/KTB-04-parsing.kt
@@ -1,2 +1,31 @@
 package org.example
 
+import java.io.File
+import java.io.IOException
+
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0
+)
+
+fun main() {
+    val wordsFile: File = File("words.txt")
+
+    try {
+        val lines: List<String> = wordsFile.readLines()
+        for (line in lines) {
+            val parts = line.split("|")
+            if (parts.size >= 2) {
+                val correctAnswers = parts.getOrNull(2)?.toIntOrNull() ?: 0
+                val word = Word(original = parts[0], translate = parts[1], correctAnswersCount = correctAnswers)
+                println(word)
+            } else {
+                println("Пропущена некорректная строка: $line")
+            }
+        }
+    } catch (e: IOException) {
+        println("Не найден файл ${e.message}.")
+    }
+
+}
